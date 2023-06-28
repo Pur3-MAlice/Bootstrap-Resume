87f4aadd4a9e2e720b99b74512d8529eb8cd558a
Pur3-MAlice <aliceford98@live.co.uk>
2023-06-28 16:16:11 +0000

    adds when/then functions for error handling

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -12,4 +12,22 @@ function fetchGitHubInformation(event) {
         </div>`
     );
 
+    $.when(
+        $getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        function(response) {
+            let userData = response;
+            $("#gh-user-data").html(userInformationHTML(userData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h5>No info found for this user</h5>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h5>ERROR: ${errorResponse.responseJSON.message}</h5>`
+                );
+            }
+        })
 }
