60e8c18576296834cb7258e24d6f4bf5b3a27ca4
Pur3-MAlice <aliceford98@live.co.uk>
2023-06-29 21:02:59 +0000

    adds sendMail function

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,1 +1,16 @@
-console.log("hello");
+function sendMail(contactForm) {
+    var templateParams = {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_request": contactForm.projectsummary.value,
+        };
+        
+    emailjs.send('gmail', 'template_f5bd9v8', templateParams)
+    .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+        console.log('FAILED...', error);
+    });
+
+    return false;
+}
